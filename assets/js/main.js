1007387a62dcb6615e6089cb42640c9511b88473
Vitalis <cernenkovitalij408@gmail.com>
2022-10-24 21:28:51 +0200

    верстка about и form

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,7 @@
+
 $(document).ready(function(){
 	$('.header-navbar__btn').on('click', function(){
 		$(this).toggleClass('header-navbar__btn--active');
-    $('.header-navbar__list').stop(true,true).slideToggle(500);
+    $('.header-navbar__list').stop(true, true).slideToggle(500);
 	});
 });
